c3a0d8a4fb9da1773e5e1e43ca12c501794c9b85
VictorSantos <victorsantos.ft18@gmail.com>
2017-11-01 21:30:47 -0200

    Criação da classe midia;

--- GerenciadorMidia/src/gerenciadores/gerPrincipal.java
+++ GerenciadorMidia/src/gerenciadores/gerPrincipal.java
@@ -1,7 +1,23 @@
 
 package gerenciadores;
 
+import java.util.ArrayList;
+import midias.Midia;
 
-public class gerPrincipal {
+
+public interface gerPrincipal {
+    
+    public abstract boolean adicionar(Midia midia);
+    
+    public abstract Midia consulta(int codigo);
+    
+    public abstract void classifica(ArrayList<Midia> midia);
+    
+    public abstract void edicao();
+    
+    public abstract boolean exclusao();
+    
+    public abstract Midia exibir();
+ 
     
 }
