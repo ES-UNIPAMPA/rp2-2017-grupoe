bbc616bad63e8f21fc54e499fe8688e9e6c09b4b
VictorSantos <victorsantos.ft18@gmail.com>
2017-11-01 21:29:46 -0200

    Criação da classe midia;

---
+++ GerenciadorMidia/src/midias/Midia.java
@@ -0,0 +1,59 @@
+package midias;
+
+public class Midia {
+
+    protected int codigo;
+    protected String caminho;
+    protected String titulo;
+    protected String descricao;
+    protected String genero;
+    protected String autores;
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getCaminho() {
+        return caminho;
+    }
+
+    public void setCaminho(String caminho) {
+        this.caminho = caminho;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String getAutores() {
+        return autores;
+    }
+
+    public void setAutores(String autores) {
+        this.autores = autores;
+    }
+}
